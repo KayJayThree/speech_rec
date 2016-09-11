223e2fc817bdcf6b7a9fbf65b8c248c8461e24b1
Ron Stengel <rkstengel@gmail.com>
2016-09-10 19:25:57 -0700

    First updatge

---
+++ speech_rec/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace speech_rec
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnSpeak = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnResume = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.cbVoice = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(798, 416);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // btnSpeak
+            // 
+            this.btnSpeak.Location = new System.Drawing.Point(35, 435);
+            this.btnSpeak.Name = "btnSpeak";
+            this.btnSpeak.Size = new System.Drawing.Size(75, 23);
+            this.btnSpeak.TabIndex = 1;
+            this.btnSpeak.Text = "Speak";
+            this.btnSpeak.UseVisualStyleBackColor = true;
+            this.btnSpeak.Click += new System.EventHandler(this.btnSpeak_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.Location = new System.Drawing.Point(127, 435);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(75, 23);
+            this.btnPause.TabIndex = 2;
+            this.btnPause.Text = "Pause";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // btnResume
+            // 
+            this.btnResume.Location = new System.Drawing.Point(220, 435);
+            this.btnResume.Name = "btnResume";
+            this.btnResume.Size = new System.Drawing.Size(75, 23);
+            this.btnResume.TabIndex = 3;
+            this.btnResume.Text = "Resume";
+            this.btnResume.UseVisualStyleBackColor = true;
+            this.btnResume.Click += new System.EventHandler(this.btnResume_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(317, 435);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 4;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // cbVoice
+            // 
+            this.cbVoice.FormattingEnabled = true;
+            this.cbVoice.Location = new System.Drawing.Point(455, 435);
+            this.cbVoice.Name = "cbVoice";
+            this.cbVoice.Size = new System.Drawing.Size(222, 21);
+            this.cbVoice.TabIndex = 5;
+            this.cbVoice.SelectedIndexChanged += new System.EventHandler(this.cbVoice_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(833, 556);
+            this.Controls.Add(this.cbVoice);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnResume);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.btnSpeak);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnSpeak;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button btnResume;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.ComboBox cbVoice;
+    }
+}
+
---
+++ speech_rec/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech;
+using System.Speech.Synthesis;
+//using System.Speech.Synthesis.InstalledVoice;
+
+
+namespace speech_rec
+{
+    public partial class Form1 : Form
+    {
+
+        //SpeechSynthesizer speaker = new SpeechSynthesizer();
+        //bool flag = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //MessageBox.Show(reader.GetInstalledVoices().ToList().ToString());
+            
+            //////////////////////////////////////////
+            //below fills Listbox Voice with available voices
+            //List<string> MyVoice = new List<string>();
+            //cbVoice.Items.AddRange(MyVoice.ToArray());
+            //cbVoice.DataSource = MyVoice;
+            foreach (InstalledVoice reader in reader.GetInstalledVoices())
+            {
+                cbVoice.Items.Add(reader.VoiceInfo.Name);
+                
+            }
+
+
+        }
+
+        SpeechSynthesizer reader = new SpeechSynthesizer();
+        
+  
+        private void btnSpeak_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.Text != "")
+            {
+                reader.Dispose();
+                reader = new SpeechSynthesizer();
+                //added next line to get voice  09/10/2016  12:10am
+                
+                reader.SelectVoice(cbVoice.Text);
+                reader.SpeakAsync(richTextBox1.Text);
+                
+            }
+            else
+            {
+                MessageBox.Show("Please enter some text!");
+            }
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            if (reader != null)
+            {
+                if (reader.State == SynthesizerState.Speaking)
+                {
+                    reader.Pause();
+                }
+            }
+        }
+
+        private void btnResume_Click(object sender, EventArgs e)
+        {
+            if (reader != null)
+            {
+                if (reader.State == SynthesizerState.Paused)
+                {
+                    reader.Resume();
+                }
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if(reader != null)
+            {
+                reader.Dispose();
+            }
+        }
+        
+        private void cbVoice_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //foreach (InstalledVoice reader in reader.GetInstalledVoices())
+            //{
+            //    cbVoice.Items.Add(reader.VoiceInfo.Name);
+            //}
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
